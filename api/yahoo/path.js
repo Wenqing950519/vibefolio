5f4f577b0658a2f4ae03f82b6b1b70b1f8176ff1
Wenqing950519 <eason9504315@gmail.com>
2026-05-12 12:45:59 +0800

    add vercel rewrite for yahoo proxy

--- api/yahoo/path.js
+++ api/yahoo/path.js
@@ -1,12 +1,10 @@
 export default async function handler(req, res) {
-  const path = req.url.replace('/api/yahoo', '')
-  const url = `https://query1.finance.yahoo.com${path}`
+  const fullPath = req.url.replace('/api/yahoo/path', '')
+  const url = `https://query1.finance.yahoo.com${fullPath}`
   
   try {
     const response = await fetch(url, {
-      headers: {
-        'User-Agent': 'Mozilla/5.0',
-      }
+      headers: { 'User-Agent': 'Mozilla/5.0' }
     })
     const data = await response.json()
     res.setHeader('Access-Control-Allow-Origin', '*')
